20f445741a48a6f8af0804aae4e97835fa81fe14
Michael Mwanza <mwanza.n.m@gmail.com>
2025-04-16 16:43:22 +0200

    db settings change

--- refrigeration_api/settings.py
+++ refrigeration_api/settings.py
@@ -113,7 +113,7 @@ WSGI_APPLICATION = 'refrigeration_api.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/refrigeration_api',
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
 }
